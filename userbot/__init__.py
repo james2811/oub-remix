449b8f41a1bd89684adffa44c999fc9cf08fa003
James Phạm <52035932+james2811@users.noreply.github.com>
2020-10-18 21:38:06 +0700

    Add API Key & API Hash

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -50,8 +50,7 @@ if version_info[0] < 3 or version_info[1] < 8:
 
 # Check if the config was edited by using the already used variable.
 # Basically, its the 'virginity check' for the config file ;)
-CONFIG_CHECK = os.environ.get(
-    "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+CONFIG_CHECK = os.environ.get("", None)
 
 if CONFIG_CHECK:
     LOGS.info(
@@ -60,8 +59,8 @@ if CONFIG_CHECK:
     quit(1)
 
 # Telegram App KEY and HASH
-API_KEY = os.environ.get("API_KEY", None)
-API_HASH = os.environ.get("API_HASH", None)
+API_KEY = os.environ.get("API_KEY", "929214")
+API_HASH = os.environ.get("API_HASH", "3cb4f7b5b7fe6c2cc7968ef8d5bc9d99")
 
 # Userbot Session String
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
